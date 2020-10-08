759e3dab93d616013b6c5bc69cdb2ef96e3c94c3
MiguelMJ <miguelmejiajimenez@hotmail.com>
2020-10-08 16:09:48 +0200

    Automatic update

--- Velero-Engine/classge_1_1Entity.js
+++ Velero-Engine/classge_1_1Entity.js
@@ -4,6 +4,7 @@ var classge_1_1Entity =
     [ "~Entity", "classge_1_1Entity.html#aaa0e115c2c5d7597e6fd26396656cf8f", null ],
     [ "notify", "classge_1_1Entity.html#aee0f04e63e9d6f241680a2e2d36cabc6", null ],
     [ "notify", "classge_1_1Entity.html#a615f3561c49625b4e8bb4b32f603df64", null ],
+    [ "apply", "classge_1_1Entity.html#ad91a7c4a72a2aaa7d6b001b176e194ac", null ],
     [ "addComponentFromPtr", "classge_1_1Entity.html#a56b4b7e7db0aa1d28a05758e9bca0e1d", null ],
     [ "addComponent", "classge_1_1Entity.html#adaf21f28b620199f8642860998cc3fd9", null ],
     [ "removeComponents", "classge_1_1Entity.html#a1c90117b5a9270a0436e7b61ca5957fa", null ],
