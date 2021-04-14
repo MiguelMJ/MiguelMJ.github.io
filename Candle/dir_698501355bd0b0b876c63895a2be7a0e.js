25e885a2fcdc9d174fb8f6b960737b2a58e439e2
MiguelMJ <miguelmejiajimenez@hotmail.com>
2021-04-14 12:52:49 +0200

    update Candle doc

--- Candle/dir_698501355bd0b0b876c63895a2be7a0e.js
+++ Candle/dir_698501355bd0b0b876c63895a2be7a0e.js
@@ -2,6 +2,7 @@ var dir_698501355bd0b0b876c63895a2be7a0e =
 [
     [ "geometry", "dir_2d1cddf0c83d37b9fa7ceea74b463b69.html", "dir_2d1cddf0c83d37b9fa7ceea74b463b69" ],
     [ "Candle.hpp", "Candle_8hpp.html", null ],
+    [ "Constants.hpp", "Constants_8hpp.html", "Constants_8hpp" ],
     [ "DirectedLight.hpp", "DirectedLight_8hpp.html", [
       [ "DirectedLight", "classcandle_1_1DirectedLight.html", "classcandle_1_1DirectedLight" ]
     ] ],
